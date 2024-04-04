a8e31dc4bf7aa935bcd626bb643af53bd599647f
MartinLacsamana <martinlacsamana@gmail.com>
2024-04-04 01:46:04 -0700

    First iterations is basically done. Code is undocumented and messy, but will go in and modularize components another day

---
+++ my-app/app/page.tsx
@@ -0,0 +1,158 @@
+
+import Link from "next/link";
+import ElavizeComponent from "@/components/elavize"
+import AWSComponent from "@/components/aws";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center ">
+        <div className="container px-4 py-12 md:py-16 lg:py-20">
+          <div className="grid gap-6 md:gap-8 lg:gap-10 items-center justify-center text-center">
+              <div className="space-y-3">
+                  <p className="text-sm font-semibold tracking-wide sm:text-base md:text-sm/looser">Introducing</p>
+                  <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl/looser">Martin Lacsamana</h1>
+                  <div className="mx-auto max-w-3xl text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                    <p>Thrill-Seeker. Aspiring Engineer.</p>
+                  </div>
+
+                  
+              </div>
+
+              <div className="flex flex-col justify-center items-center -mt-8 mb-2">
+                <img
+                  alt="Gallery Image 1"
+                  className="overflow-hidden rounded-md object-cover w-full max-w-xs"
+                  src="/MartinAvatarNoBackground.png"
+                />
+                <span className="text-xs text-gray-400 mt-2">(AI-generated)</span>
+
+              </div>
+              
+
+            
+
+
+
+
+              <div className="flex justify-center mb-12">
+                <Link href="#traveling" className="bg-black text-white font-semibold py-4 px-6 rounded-full hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 shadow-lg transition ease-in duration-200 text-center text-base">
+               
+                    Explore My Purpose
+               
+                </Link>
+              </div>
+
+        
+
+            <div className="space-y-3 pt-12 " id="traveling">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">They say a picture is worth a thousand words.</h2>
+                <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                  Let's test that theory with a couple.
+                </p>
+            </div>
+
+            {/* Images section*/}
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <div className="border border-gray-300 pb-2 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 transition-transform duration-50 ease-in-out hover:scale-105">
+                  <img
+                    alt="Gallery Image 1"
+                    className="aspect-video overflow-hidden rounded-t-lg object-cover object-top"
+                    src="/Skydiving1.jpg"
+                  />
+                  <p className="text-sm tracking-wide sm:text-base md:text-sm italic font-medium text-gray-600 mt-2 px-4 py-2">Skydiving in California</p>
+              </div>
+              
+              <div className="border border-gray-300 pb-2 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 transition-transform duration-50 ease-in-out hover:scale-105">
+                <img
+                  alt="Gallery Image 2"
+                  className="aspect-video overflow-hidden rounded-t-lg object-cover object-top"
+                  src="/WhaleShark.JPG"
+                />
+                <p className="text-sm tracking-wide sm:text-base md:text-sm italic font-medium text-gray-600 mt-2 px-4 py-2">Swimming w/ Whale Sharks</p>
+              </div>
+
+              <div className="border border-gray-300 pb-2 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 transition-transform duration-50 ease-in-out hover:scale-105">
+                <img
+                  alt="Gallery Image 3"
+                  className="aspect-video overflow-hidden rounded-t-lg object-cover object-top"
+                  src="/Ceros.jpg"
+                />
+                <p className="text-sm tracking-wide sm:text-base md:text-sm italic font-medium text-gray-600 mt-2 px-4 py-2">Presenting Internship Recap</p>
+              </div>
+            
+            </div>
+
+
+            <div className=" w-full mt-6">
+              <div className="flex flex-col justify-center items-center shadow-md rounded-lg border-2 rounded-lg p-6 space-y-4">
+
+                  <div>
+                      <p className="md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-800">
+                        <span className="font-semibold">At first glance, these are just regular photos. </span>
+                        In every snapshot of my life's journey, whether it's leaping from a plane, exploring the depths of the
+                        ocean, or sharing knowledge with peers, the common thread has been a willingness to embrace fear and take
+                        risks. This philosophy has not only shaped my personal adventures but has also been a driving force in my
+                        professional growth as an engineer. By stepping out of my comfort zone and facing challenges head-on, I've
+                        learned that the most rewarding experiences come from the moments when we dare to venture beyond the
+                        familiar.
+                      </p>
+                  </div>
+                  
+
+                  {/* Put a graphic here on growth*/}
+                  <div>
+                      <img
+                        alt="Gallery Image 3"
+                        className="aspect-video overflow-hidden rounded-md shadow-md object-cover"
+                        src="DominoEffect2.png"
+                      />
+                      <p className="text-sm tracking-wide sm:text-base md:text-sm/looser">Short Description</p>
+                  </div>
+              </div>
+            </div>
+
+
+
+           
+
+
+          </div>
+        </div>
+        
+     
+
+
+
+
+
+
+
+     
+    
+        
+   
+      <div className="container py-4 md:py-4">
+        <div className="grid gap-6 md:gap-8 lg:gap-10 items-center justify-center text-center">
+          <div className="space-y-3">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">My Latest Domino Takedowns</h2>
+            <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+              Do the next scariest thing.
+            </p>
+          </div>
+          
+         
+        </div>
+
+        <ElavizeComponent/>
+        <AWSComponent/>
+      </div>
+
+
+
+
+
+
+
+    </div>
+  );
+}
---
+++ my-app/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      scrollBehavior: {
+        "smooth-scroll": "smooth"
+      },
+      colors: {
+        'elavize-custom': '#BC58FF',
+        'amazon-custom': '#1399FF',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
