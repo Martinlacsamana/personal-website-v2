44dd73ad0eb1785aebbe4a3f145ba830eadd1f7e
Martin Lacsamana <martinlacsamana@Martins-MacBook-Air-2.local>
2024-09-16 18:53:15 -0700

    Fixed project card spacing

--- components/Landing/Projects/ProjectCard.tsx
+++ components/Landing/Projects/ProjectCard.tsx
@@ -17,27 +17,29 @@ interface Props {
 
 const ProjectCard = ({hook, position, children, image, links}: Props) => {
     return (
-        <div className="flex flex-col w-full max-w-sm mx-auto bg-transparent border border-gray-700 rounded-lg overflow-hidden shadow-lg hover:border-gray-500 transition-all duration-300">
+        <div className="flex flex-col w-full max-w-sm mx-auto bg-transparent border border-gray-700 rounded-lg overflow-hidden shadow-lg hover:border-gray-500 transition-all duration-300 h-[600px]">
             {image && (
-                <div className="w-full h-64 relative">
+                <div className="w-full h-64 relative flex-shrink-0">
                    <Image 
-            src={image} 
-            alt="Project image" 
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            style={{ objectFit: 'cover', objectPosition: 'center top' }}
-        />
+                        src={image} 
+                        alt="Project image" 
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        style={{ objectFit: 'cover', objectPosition: 'center top' }}
+                    />
                 </div>
             )}
-            <div className="p-5 space-y-4 flex flex-col justify-between h-full">
-                <div>
-                    <h2 className="text-2xl font-semibold text-white mb-2">{hook}</h2>
-                    <h3 className="text-sm text-gray-400 mb-4">{position}</h3>
-                    <div className="text-gray-300 text-sm">
-                        {children}
+            <div className="p-5 flex flex-col justify-between h-full">
+                <div className="space-y-4 overflow-y-auto flex-grow">
+                    <div>
+                        <h2 className="text-2xl font-semibold text-white mb-2">{hook}</h2>
+                        <h3 className="text-sm text-gray-400 mb-4">{position}</h3>
+                        <div className="text-gray-300 text-sm">
+                            {children}
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-wrap gap-4">
+                <div className="flex flex-wrap gap-4 mt-4">
                     {links.map((link, index) => (
                         <Link key={index} href={link.url} target="_blank" rel="noopener noreferrer">
                             <span className="text-blue-400 hover:text-blue-300 transition-colors duration-300 cursor-pointer">
